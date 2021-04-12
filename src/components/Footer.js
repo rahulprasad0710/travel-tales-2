9704f5256ab1e44a96f89a2346c65912dbed772d
rahulprasad0710 <raulshah2021@hotmail.com>
2021-04-13 02:37:08 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+const Footer = () => {
+  return (
+    <div className="page-footer">
+      <h3>
+        &copy; 2021 <span className="text-red">HAPPY</span>{" "}
+        <span className="text-yellow">MEAL</span>
+      </h3>
+      <h3>
+        Built with <a href="https://www.gatsbyjs.com/">Gatsby</a>&nbsp; By:{" "}
+        {"  "}
+        <span>
+          <a href="https://www.google.com/"> Rahul</a>
+        </span>
+      </h3>
+      <h4>All Right Reserved.</h4>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,98 @@
+import React, { useState } from "react"
+import { Link } from "gatsby"
+
+import "./navbar.css"
+
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false)
+
+  const toggleMenuHandler = () => {
+    setToggleMenu(!toggleMenu)
+    console.log(toggleMenu)
+  }
+
+  return (
+    <nav className="navbar">
+      <div className="navbar_nav-container container-lg">
+        <div className="navbar_brand-toggle">
+          <Link className="navbar_brandName" to="/">
+            <span className=""> TRAVEL</span>
+            <span className=""> TALES</span>
+          </Link>
+          <div className="navbar_toogle-btns " onClick={toggleMenuHandler}>
+            <div
+              className={
+                toggleMenu ? "navbar_burger navbar_burger-1" : "navbar_burger"
+              }
+            ></div>
+            <div
+              className={
+                toggleMenu ? "navbar_burger navbar_burger-2" : "navbar_burger "
+              }
+            ></div>
+            <div
+              className={
+                toggleMenu ? "navbar_burger navbar_burger-3" : "navbar_burger "
+              }
+            ></div>
+          </div>
+        </div>
+
+        <div
+          className={
+            toggleMenu
+              ? "navbar_nav-menu-div navbar_nav-menu-div-mobile navbar_menu-active"
+              : "navbar_nav-menu-div navbar_nav-menu-div-mobile "
+          }
+        >
+          <ul className="navbar_nav-menu">
+            <li
+              onClick={() => setToggleMenu(false)}
+              className="navbar_nav-menu-item"
+            >
+              <Link
+                activeClassName="navLink-active"
+                className="navbar_nav-menu-item-link"
+                to="/"
+              >
+                HOME
+              </Link>
+            </li>
+            <li className="navbar_nav-menu-item">
+              <Link
+                onClick={() => setToggleMenu(false)}
+                className="navbar_nav-menu-item-link"
+                activeClassName="navLink-active"
+                to="/category/"
+              >
+                CATEGORY
+              </Link>
+            </li>
+            <li className="navbar_nav-menu-item">
+              <Link
+                onClick={() => setToggleMenu(false)}
+                className="navbar_nav-menu-item-link"
+                activeClassName="navLink-active"
+                to="/about/"
+              >
+                ABOUT
+              </Link>
+            </li>
+            <li className="navbar_nav-menu-item">
+              <Link
+                onClick={() => setToggleMenu(false)}
+                className="navbar_nav-menu-item-link"
+                activeClassName="navLink-active"
+                to="/gallery"
+              >
+                GALLLERY
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
